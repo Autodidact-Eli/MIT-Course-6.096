a9aa0cf82a40a1799e29a10796a29574c451918d
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-09-21 15:02:00 -0400

    need to finish

---
+++ Lab 1/3.3.cpp
@@ -0,0 +1,10 @@
+/*
+3.3 Prime Numbers
+Write a program to read a number N from the user and then find the first N primes. A
+prime number is a number that only has two divisors, one and itself.
+*/
+
+// TODO: Finish
+int main() {
+	return 0;
+}
