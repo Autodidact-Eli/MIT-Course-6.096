55f92ac10cea7feed76293f90f14ab5f87317704
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-09-21 18:55:04 -0400

    fixed spacing

--- Lab 1/3.3.cpp
+++ Lab 1/3.3.cpp
@@ -3,7 +3,6 @@
 Write a program to read a number N from the user and then find the first N primes. A
 prime number is a number that only has two divisors, one and itself.
 */
-
 #include <iostream>
 
 using namespace std;
