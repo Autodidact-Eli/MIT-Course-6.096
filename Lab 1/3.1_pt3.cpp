1bb2650075f5222b8b566ae5de89a04ee9db3784
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-09-21 14:34:35 -0400

    more exercises

---
+++ Lab 1/3.1_pt3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+/*
+3. Below is a sample program. Use it to answer the following question: Suppose an
+identifier has two different declarations, one in an outer block and one in a nested
+inner block. If the name is accessed within the inner block, which declaration is used?
+*/
+int main() {
+	int arg1;
+	arg1 = -1;
+	{
+		char arg1 = 'A';
+		cout << arg1 << "\n";
+	}
+	return 0;
+}
+
+/*
+The declaration used inside the inner block is accessed.
+*/
---
+++ Lab 1/3.1_pt5.cpp
@@ -0,0 +1,17 @@
+/*
+5. Below is a sample program that will not compile. Why not? By moving which line
+can we get the code to compile?
+*/
+
+using namespace std;
+
+int main() {
+	cout << "Hello, World!\n";
+	return 0;
+}
+
+#include <iostream>
+
+/*
+If we moved the #include <iostream> to the top, this program will compile.
+*/
