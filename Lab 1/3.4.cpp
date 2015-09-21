9a24d8ac9fbf14b43839779486c5d32f25539d5b
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-09-21 15:05:22 -0400

    added comments

--- Lab 1/3.4.cpp
+++ Lab 1/3.4.cpp
@@ -5,6 +5,15 @@ Write a program that loops indefinitely. In each iteration of the loop, read in 
 5 if N is nonnegative and divisible by
 5, and -1 otherwise. Use the ternary operator (?:) to accomplish this. (Hint: the modulus
 operator may be useful.)
+
+3.4.2 continue
+Modify the code from 3.4.1 so that if the condition fails, nothing is printed. Use an if and
+a continue command (instead of the ternary operator) to accomplish this.
+
+
+3.4.3 break
+Modify the code from 3.4.2 to let the user break out of the loop by entering -1 or any negative
+number. Before the program exits, output the string “Goodbye!”.
 */
 #include <iostream>
 
