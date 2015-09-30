9e2524c0a056c3ea609f0817d482b6f2fbb341b4
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-09-30 12:25:46 -0400

    comments and brackets

--- Lab_1/3.3.cpp
+++ Lab_1/3.3.cpp
@@ -1,13 +1,13 @@
-/*
-3.3 Prime Numbers
-Write a program to read a number N from the user and then find the first N primes. A
-prime number is a number that only has two divisors, one and itself.
-*/
+// 3.3 Prime Numbers
+// Write a program to read a number N from the user and then find the first N primes. A
+// prime number is a number that only has two divisors, one and itself.
+
 #include <iostream>
 
 using namespace std;
 
-int main() {
+int main()
+{
 	int N;
 	cout << "Enter an N: ";
 	cin >> N;
--- Lab_1/3.4.cpp
+++ Lab_1/3.4.cpp
@@ -1,25 +1,25 @@
-/*
-3.4.1 Ternary operator
-Write a program that loops indefinitely. In each iteration of the loop, read in an integer N
-(declared as an int) that is inputted by a user, output N
-5 if N is nonnegative and divisible by
-5, and -1 otherwise. Use the ternary operator (?:) to accomplish this. (Hint: the modulus
-operator may be useful.)
+// 3.4.1 Ternary operator
+// Write a program that loops indefinitely. In each iteration of the loop, read in an integer N
+// (declared as an int) that is inputted by a user, output N
+// 5 if N is nonnegative and divisible by
+// 5, and -1 otherwise. Use the ternary operator (?:) to accomplish this. (Hint: the modulus
+// operator may be useful.)
 
-3.4.2 continue
-Modify the code from 3.4.1 so that if the condition fails, nothing is printed. Use an if and
-a continue command (instead of the ternary operator) to accomplish this.
+// 3.4.2 continue
+// Modify the code from 3.4.1 so that if the condition fails, nothing is printed. Use an if and
+// a continue command (instead of the ternary operator) to accomplish this.
 
 
-3.4.3 break
-Modify the code from 3.4.2 to let the user break out of the loop by entering -1 or any negative
-number. Before the program exits, output the string “Goodbye!”.
-*/
+// 3.4.3 break
+// Modify the code from 3.4.2 to let the user break out of the loop by entering -1 or any negative
+// number. Before the program exits, output the string “Goodbye!”.
+
 #include <iostream>
 
 using namespace std;
 
-int main() {
+int main()
+{
 	while (true) {
 		int N;
 		cin >> N;
