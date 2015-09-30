a8736e3e85b8b418577e03f27f3f3863422d121d
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-09-30 12:31:20 -0400

    comments and brackets

--- Lab_2/4.5.cpp
+++ Lab_2/4.5.cpp
@@ -1,7 +1,7 @@
 // Write a single sum funtion capable of handling an arbitrary number of integers
 // It should take two arguments, include a loop, and return an integer
 // (Hint: What data types can you use to represent an arbitrarly large set of
-//       integers in two arguments?)
+//        integers in two arguments?)
 int sum(const int[] numbers, const int numbers_length)
 {
 	int total_sum = 0;
