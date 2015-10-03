329bc4640c9010cae78f07bd4fb065b0000e6b98
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:37:45 -0400

    random program

--- Miscellaneous/simpleFunc.cpp
+++ Miscellaneous/simpleFunc.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void f(const int a = 5)
 {
-	cout << a*2 << " ";
+  cout << a*2 << " ";
 }
 
 int a = 123;
@@ -12,14 +12,14 @@ int a = 123;
 // What will the following program print out?
 int main()
 {
-	f(1);
-	f(a);
-	int b = 3;
-	f(b);
-	int a = 4;
-	f(a);
-	f();
-	cout << '\n';
+  f(1);
+  f(a);
+  int b = 3;
+  f(b);
+  int a = 4;
+  f(a);
+  f();
+  cout << '\n';
 }
 // The program prints out
 // 2 246 6 8 10
