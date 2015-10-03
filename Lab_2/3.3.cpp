15793c4e6acb265ee3d547f325d4b1efd693ecd3
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:31:55 -0400

    3.3

--- Lab_2/3.3.cpp
+++ Lab_2/3.3.cpp
@@ -4,21 +4,21 @@ using namespace std;
 // Start function
 void doubleNumber(int num)
 {
-	num *= 2;
+  num *= 2;
 }
 
 // Fixed function, pass-by-reference
 void doubleNumber(int &num)
 {
-	num *= 2;
+  num *= 2;
 }
 
 int main()
 {
-	int num = 35;
-	doubleNumber(num);
-	cout << num << '\n'; // should print 70
-	return 0;
+  int num = 35;
+  doubleNumber(num);
+  cout << num << '\n'; // should print 70
+  return 0;
 }
 // At first the program prints 35
 // What we can do is make num a pass-by-reference parameter
