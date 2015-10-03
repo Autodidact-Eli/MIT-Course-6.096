f8506e7b50300205148e77599715541bdb67c1bc
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:32:35 -0400

    3.6

--- Lab_2/3.6.cpp
+++ Lab_2/3.6.cpp
@@ -5,11 +5,11 @@ const int ARRAY_LEN = 10;
 
 int main()
 {
-	int arr[ARRAY_LEN] = {10}; // Note implicit initialization of other elements
+  int arr[ARRAY_LEN] = {10}; // Note implicit initialization of other elements
 
-	int *xPtr = arr, yPtr = arr + ARRAY_LEN - 1; // ERROR HERE: should be *yPtr
-	int *xPtr = arr, *yPtr = arr + ARRAY_LEN - 1; // FIXED
+  int *xPtr = arr, yPtr = arr + ARRAY_LEN - 1; // ERROR HERE: should be *yPtr
+  int *xPtr = arr, *yPtr = arr + ARRAY_LEN - 1; // FIXED
 
-	cout << *xPtr << ' ' << *yPtr << '\n'; // Should output 10 0
-	return 0;
+  cout << *xPtr << ' ' << *yPtr << '\n'; // Should output 10 0
+  return 0;
 }
