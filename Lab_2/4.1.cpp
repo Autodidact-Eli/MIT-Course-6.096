0d45885eb5cb843e5d9cb033dfb73426f51ca80f
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:33:21 -0400

    4.1

--- Lab_2/4.1.cpp
+++ Lab_2/4.1.cpp
@@ -5,20 +5,20 @@ using namespace std;
 // Also write the equivalent function for taking the sum of two doubles.
 int sum(const int x, const int y)
 {
-	return x + y;
+  return x + y;
 }
 
 double sum(const double x, const double y)
 {
-	return x + y;
+  return x + y;
 }
 
 int main()
 {
-	int a = 5, b = 5;
-	double c = 3.3, d = 3.3;
+  int a = 5, b = 5;
+  double c = 3.3, d = 3.3;
 
-	cout << "5 + 5 = " << sum(a,b) << '\n';
-	cout << "3.5 + 3.5 = " << sum(c,d) << '\n';
-	return 0;
+  cout << "5 + 5 = " << sum(a,b) << '\n';
+  cout << "3.5 + 3.5 = " << sum(c,d) << '\n';
+  return 0;
 }
