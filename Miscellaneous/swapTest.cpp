af420dc0cd66c73aa14081f238285353e9bbea52
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:36:50 -0400

    demo pass-by-reference

--- Miscellaneous/swapTest.cpp
+++ Miscellaneous/swapTest.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 
 void swap(int &a, int &b) {
-	int temp = a;
-	a = b;
-	b = temp;
+  int temp = a;
+  a = b;
+  b = temp;
 }
 
 // Program to demonstrate pass-by-reference using a swap method
 int main() {
-	int x, y;
+  int x, y;
 
-	cout << "Enter a value for x: ";
-	cin >> x;
+  cout << "Enter a value for x: ";
+  cin >> x;
 
-	cout << "Enter a value for y: ";
-	cin >> y;
+  cout << "Enter a value for y: ";
+  cin >> y;
 
-	cout << "The values before swap, x = " << x << " and y = " << y << '\n';
-	swap(x,y);
-	cout << "The values after swap, x = " << x << " and y = " << y << '\n';
-	return 0;
+  cout << "The values before swap, x = " << x << " and y = " << y << '\n';
+  swap(x,y);
+  cout << "The values after swap, x = " << x << " and y = " << y << '\n';
+  return 0;
 }
