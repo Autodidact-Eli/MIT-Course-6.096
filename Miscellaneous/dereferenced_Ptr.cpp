a98ea4856bcbca22ee19688ea53130d2361493b9
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:40:24 -0400

    another random program

--- Miscellaneous/dereferenced_Ptr.cpp
+++ Miscellaneous/dereferenced_Ptr.cpp
@@ -2,11 +2,11 @@
 // the function returns is invalid
 int *myFunc()
 {
-	int phantom = 4;
-	return &phantom;
+  int phantom = 4;
+  return &phantom;
 }
 
 int y;
 int &x = y; // Makes 'x' a reference to, or alias of 'y'
-						// After these declarations, changing 'x' will change 'y' and
-						// vice versa, because they are two names for the same thing
+            // After these declarations, changing 'x' will change 'y' and
+            // vice versa, because they are two names for the same thing
