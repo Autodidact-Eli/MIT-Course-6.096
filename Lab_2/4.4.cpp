18e53a19fddeaa77463c1202197d2b98a003cd38
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:33:44 -0400

    4.4

--- Lab_2/4.4.cpp
+++ Lab_2/4.4.cpp
@@ -7,19 +7,19 @@ using namespace std;
 
 int sum(const int num1, const int num2, const int num3, const int num4 = 0)
 {
-	return num1 + num2 + num3 + num4;
+  return num1 + num2 + num3 + num4;
 }
 
 // From 3.3
 int sum(const int num1, const int num2, const int num3)
 {
-	return num1 + num2 + num3;
+  return num1 + num2 + num3;
 }
 
 int main()
 {
-	cout << sum(3,5,7) << '\n';
-	return 0;
+  cout << sum(3,5,7) << '\n';
+  return 0;
 }
 
 // Q: What would happen if you put both this definition and the 3-argument
