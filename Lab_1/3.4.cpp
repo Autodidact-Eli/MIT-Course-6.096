749f7420a368f8deaf8b1a97204405954df77798
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:25:07 -0400

    3.4

--- Lab_1/3.4.cpp
+++ Lab_1/3.4.cpp
@@ -20,15 +20,15 @@ using namespace std;
 
 int main()
 {
-	while (true) {
-		int N;
-		cin >> N;
-		if (N == -1) {
-			cout << "Goodbye!\n";
-			break;
-		}
-		cout << ((N > 0 && N % 5 == 0) ? N/5 : -1) << '\n';
-	}
+  while (true) {
+    int N;
+    cin >> N;
+    if (N == -1) {
+      cout << "Goodbye!\n";
+      break;
+    }
+    cout << ((N > 0 && N % 5 == 0) ? N/5 : -1) << '\n';
+  }
 
-	return 0;
+  return 0;
 }
