b076295a5ba74d7ae9111cbc4c04808ed047c128
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:39:25 -0400

    another random program

--- Miscellaneous/pointerTest.cpp
+++ Miscellaneous/pointerTest.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 void squareByPtr(int *numPtr) {
-	*numPtr = (*numPtr) * (*numPtr);
+  *numPtr = (*numPtr) * (*numPtr);
 }
 
 int main() {
-	int x = 5; 					// starting point
+  int x = 5;          // starting point
 
-	cout << x << '\n'; 	// prints 5
-	cout << &x << '\n'; // prints memory address of x
+  cout << x << '\n';  // prints 5
+  cout << &x << '\n'; // prints memory address of x
 
-	squareByPtr(&x);
+  squareByPtr(&x);
 
-	cout << x << '\n'; 	// prints 25
-	cout << &x << '\n'; // prints memory address of x
+  cout << x << '\n';  // prints 25
+  cout << &x << '\n'; // prints memory address of x
 
-	return 0;
+  return 0;
 }
