f951e8985f79c1d38579cc59c98271e5ac217ad4
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:22:21 -0400

    3.1 part 3

--- Lab_1/3.1_pt3.cpp
+++ Lab_1/3.1_pt3.cpp
@@ -8,13 +8,13 @@ using namespace std;
 
 int main()
 {
-	int arg1;
-	arg1 = -1;
-	{
-		char arg1 = 'A';
-		cout << arg1 << "\n";
-	}
-	return 0;
+  int arg1;
+  arg1 = -1;
+  {
+    char arg1 = 'A';
+    cout << arg1 << "\n";
+  }
+  return 0;
 }
 
 // The declaration used inside the inner block is accessed.
