c1f2ec84ca717161db9a353e6596861d775654da
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:34:09 -0400

    5.1

--- Lab_2/5.1.cpp
+++ Lab_2/5.1.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <cstdlib> 	// C standard library
-										// defines rand(), srand(), RAND_MAX
-#include <cmath>		// C time functions - defines time()
+#include <cstdlib>  // C standard library
+                    // defines rand(), srand(), RAND_MAX
+#include <cmath>    // C time functions - defines time()
 
 double pi(int num_dart_throws)
 {
-	int seed = srand(time(num_dart_throws));
+  int seed = srand(time(num_dart_throws));
 }
 
 int main()
 {
-	double x_coord = rand(0,1);
-	double y coord = rand(0,1);
+  double x_coord = rand(0,1);
+  double y coord = rand(0,1);
 
-	int count = 0; // how many darts have ended up inside the circle
+  int count = 0; // how many darts have ended up inside the circle
 }
