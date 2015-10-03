d890954230ae68f6e2b780d61d407d6adfcd8f53
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:41:54 -0400

    basic program with arrays

--- Miscellaneous/arrayTest.cpp
+++ Miscellaneous/arrayTest.cpp
@@ -2,27 +2,29 @@
 
 using namespace std;
 
-void printArray(int array[], int size) {
-	for (int i = 0; i < size; ++i) {
-		cout << "array[" << i << "] = " << array[i] << '\n';
-	}
+void printArray(int array[], int size)
+{
+  for (int i = 0; i < size; ++i) {
+    cout << "array[" << i << "] = " << array[i] << '\n';
+  }
 }
 
-int main() {
-	int size = 0;
+int main()
+{
+  int size = 0;
 
-	cout << "Enter an integer for size: ";
-	cin >> size;
+  cout << "Enter an integer for size: ";
+  cin >> size;
 
-	int array[size]; // declare an array with the input as its size
+  int array[size]; // declare an array with the input as its size
 
-	// fill array
-	for (int i = 0; i < size; ++i) {
-		cout << "Enter element: ";
-		cin >> array[i];
-	}
+  // fill array
+  for (int i = 0; i < size; ++i) {
+    cout << "Enter element: ";
+    cin >> array[i];
+  }
 
-	printArray(array, size);
+  printArray(array, size);
 
-	return 0;
+  return 0;
 }
