d9b12ea269f55cd080b55b741ed717f419d9c520
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:34:22 -0400

    6.1

--- Lab_2/6.1.cpp
+++ Lab_2/6.1.cpp
@@ -6,15 +6,15 @@ using namespace std;
 // the string ", " between elements (but not after the last element)
 void printArray(const int array[], const int length)
 {
-	for (int i = 0; i < length - 1; ++i) {
-		cout << array[i] << ", ";
-	}
-	cout << array[length-1] << '\n';
+  for (int i = 0; i < length - 1; ++i) {
+    cout << array[i] << ", ";
+  }
+  cout << array[length-1] << '\n';
 }
 
 int main()
 {
-	int array[] = {6,0,9,6};
-	printArray(array, 4);
-	return 0;
+  int array[] = {6,0,9,6};
+  printArray(array, 4);
+  return 0;
 }
