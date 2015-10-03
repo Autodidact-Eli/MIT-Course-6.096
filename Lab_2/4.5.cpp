ae757786d39428f1c73617ba001b1ba577650b28
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:33:55 -0400

    4.5

--- Lab_2/4.5.cpp
+++ Lab_2/4.5.cpp
@@ -4,20 +4,20 @@
 //        integers in two arguments?)
 int sum(const int[] numbers, const int numbers_length)
 {
-	int total_sum = 0;
-	for (int i = 0; i < numbers_length; ++i) {
-		total_sum += numbers[i];
-	}
-	return total_sum;
+  int total_sum = 0;
+  for (int i = 0; i < numbers_length; ++i) {
+    total_sum += numbers[i];
+  }
+  return total_sum;
 }
 
 // 4.6, now wrtie the same function above using recursion instead of a loop
 // (Hint: Think about how pointer arithmetic may help)
 int sum(const int[] numbers, const int numbers_length)
 {
-	if (numbers_length == 0) { // base case
-		return 0;
-	} else { 	// TODO: implement recursive call
+  if (numbers_length == 0) { // base case
+    return 0;
+  } else {  // TODO: implement recursive call
 
-	}
+  }
 }
