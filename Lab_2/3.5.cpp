38ac954af18e102043504337973796f450a1b9de
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:32:21 -0400

    3.5

--- Lab_2/3.5.cpp
+++ Lab_2/3.5.cpp
@@ -4,17 +4,17 @@ using namespace std;
 // Start(Bad) function, function needs a third parameter
 int sum(const int x, const int y)
 {
-	return x + y;
+  return x + y;
 }
 
 // Fixed function, added third parameter
 int sum(const int x, const int y, const int z)
 {
-	return x + y + z;
+  return x + y + z;
 }
 
 int main()
 {
-	cout << sum(1,2,3) << '\n'; // should print 6
-	return 0;
+  cout << sum(1,2,3) << '\n'; // should print 6
+  return 0;
 }
