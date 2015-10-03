3bc64bc02cd3221dd99608c9e00bb248e8af66ae
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 23:34:32 -0400

    6.2

--- Lab_2/6.2.cpp
+++ Lab_2/6.2.cpp
@@ -5,31 +5,31 @@ using namespace std;
 // Prints the elements in the array
 void printArray(const int array[], const int length)
 {
-	for (int i = 0; i < length; ++i) {
-		cout << array[i];
-		if (i < length - 1)
-			cout << ", ";
-	}
-	cout << '\n';
+  for (int i = 0; i < length; ++i) {
+    cout << array[i];
+    if (i < length - 1)
+      cout << ", ";
+  }
+  cout << '\n';
 }
 
 // Prints out the elements in the array in reversed order
 void reverseArray(const int array[], const int length)
 {
-	for (int i = length-1; i >= 0; --i) {
-		cout << array[i];
-		if (i > 0)
-			cout << ", ";
-	}
-	cout << '\n';
+  for (int i = length-1; i >= 0; --i) {
+    cout << array[i];
+    if (i > 0)
+      cout << ", ";
+  }
+  cout << '\n';
 }
 
 int main()
 {
-	int array[] = {1,2,3,4,5};
-	cout << "Elements before reverse: ";
-	printArray(array,5);
-	cout << "Elements after reverse: ";
-	reverseArray(array,5);
-	return 0;
+  int array[] = {1,2,3,4,5};
+  cout << "Elements before reverse: ";
+  printArray(array,5);
+  cout << "Elements after reverse: ";
+  reverseArray(array,5);
+  return 0;
 }
